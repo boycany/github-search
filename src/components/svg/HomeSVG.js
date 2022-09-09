e3e53a340561938686e6108c81247f4671f375b8
boycany <yurayurateiko@gmail.com>
2022-09-10 01:16:13 +0800

    change a tag to Link

--- src/components/svg/HomeSVG.js
+++ src/components/svg/HomeSVG.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const HomeSVG = () => {
   const svg_style = {
@@ -14,7 +14,7 @@ const HomeSVG = () => {
   };
 
   return (
-    <a href="/" alt="">
+    <Link to="/">
       <svg
         xmlns="http://www.w3.org/2000/svg"
         width="32"
@@ -34,7 +34,7 @@ const HomeSVG = () => {
           d="M7.293 1.5a1 1 0 0 1 1.414 0l6.647 6.646a.5.5 0 0 1-.708.708L8 2.207 1.354 8.854a.5.5 0 1 1-.708-.708L7.293 1.5z"
         />{" "}
       </svg>
-    </a>
+    </Link>
   );
 };
 
